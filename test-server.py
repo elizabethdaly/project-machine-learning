2de775ca27a6fd3720190268ae9f82733a2d6513
elizabethdaly <liz.m.a.daly@gmail.com>
2020-11-28 18:23:09 +0000

    Trying to figure out how theoretical power curve could be calculated

--- test-server.py
+++ test-server.py
@@ -18,10 +18,10 @@ def home():
 # Tell flask to make model 1 available at /model1
 @app.route('/api/model1')
 def model1():
-    return {"Model 1 value ": np.random.randint(1, 5)}
+    return {"value": np.random.randint(1, 5)}
 
 # Tell flask to make model 2 available at /model2
 @app.route('/api/model2')
 def model2():
-    return {"Model 2 value ": np.random.randint(10, 15)}
+    return {"value": np.random.randint(10, 15)}
 
