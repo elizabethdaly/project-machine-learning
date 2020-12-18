1f55e3a7729042ed7f24d73f151e410b3d27218d
elizabethdaly <liz.m.a.daly@gmail.com>
2020-12-18 11:14:11 +0000

    Test saving and restoring models, and do some checks of poly, NNW predictions to console

--- test-server.py
+++ test-server.py
@@ -6,6 +6,25 @@ import flask as fl
 # Numpy for numerical arrays
 import numpy as np
 
+# pickle for saving and restoring ML models
+import joblib
+
+# For restoring a keras model
+from tensorflow.keras.models import load_model
+
+# Import my polnomial regression model
+polyreg = joblib.load("poly-reg.pkl")
+# Need poly features to make a prediction for [[x, x^2, x^3]]
+print(polyreg.predict([[23, 529, 12167]])) # shape ok but manually doing poly feat
+
+# Import my SVM regression model
+svmreg = joblib.load("svm-reg.pkl")
+
+# Import my neural network model
+model = load_model("neural-nw.h5")
+# Test make a prediction - ok
+print(model.predict([[23]])) 
+
 # Create a new web application
 app = fl.Flask(__name__)
 
